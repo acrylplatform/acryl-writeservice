2c38506cc25cf6f49c5d6eb66b836eb2f32cf706
Aleksandr Volkov <avolkov05@gmail.com>
2019-08-13 15:58:37 +0700

    init with docker

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM node:10.15.3
+RUN npm install -g yarn node-gyp typescript ts-node 
+
+#CMD npm start
---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Write Service API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
